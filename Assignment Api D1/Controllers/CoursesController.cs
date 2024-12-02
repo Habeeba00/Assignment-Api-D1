150f19f257d2ed7e758fd7a6ac93e6ecd15fbab4
Habiba <Habiba@HABEEEEBA>
2024-12-03 01:29:31 +0200

    Add project files.

---
+++ Assignment Api D1/Controllers/CoursesController.cs
@@ -0,0 +1,39 @@
+﻿using Assignment_Api_D1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Assignment_Api_D1.Controllers
+{
+    [Route("api/Courses")]
+    [ApiController]
+    public class CoursesController : ControllerBase
+    {
+        static List<Courses>courses = new List<Courses>() 
+        {
+            new Courses(){Id=1,Name="C#",Duration=12},
+            new Courses(){Id=2,Name="Java",Duration=22},
+            new Courses(){Id=3,Name="CSS",Duration=8},
+            new Courses(){Id=4,Name="Html",Duration=16}
+        };
+
+        [HttpGet]
+        public List<Courses> GetAll()
+        {
+            return courses;
+        }
+
+        [HttpGet("{id}")]
+        public Courses GetById(int id) 
+        {
+            return courses.Find(c=>c.Id==id);
+        }
+
+        [HttpPost]
+        public List<Courses> Add(Courses c)
+        { 
+            courses.Add(c);
+            return courses;
+        }
+
+    }
+}
---
+++ Assignment Api D1/Models/Courses.cs
@@ -0,0 +1,9 @@
+﻿namespace Assignment_Api_D1.Models
+{
+    public class Courses
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Duration { get; set; }
+    }
+}
